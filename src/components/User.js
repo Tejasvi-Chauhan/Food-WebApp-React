dfbb9b482fd396ea662cf28066a07a177dc95b8f
Tejasvi-Chauhan <chauhantejasvi07@gmail.com>
2026-01-13 22:57:37 +0530

    updated version

--- src/components/User.js
+++ src/components/User.js
@@ -11,12 +11,8 @@ const User=(props)=>{
         <h3>Contact : 1234567890</h3>
         <h3>Profession : Developer</h3>
         <h3>Company : Microsoft</h3>
-        <h3>Experience : 5 years</h3>
-        <h3>Hobby : Coding</h3>
-        <h3>Favorite Language : JavaScript</h3>
-        <h3>GitHub : github.com/username</h3>
-        <h3>LinkedIn : linkedin.com/in/username</h3>
-        
+       
+
     
         </div>
     )
