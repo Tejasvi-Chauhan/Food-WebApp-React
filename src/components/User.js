cb8e717e83ee6abef6813865ba470122bb6d78d1
Tejasvi-Chauhan <chauhantejasvi07@gmail.com>
2026-01-16 23:50:33 +0530

    update

--- src/components/User.js
+++ src/components/User.js
@@ -7,11 +7,14 @@ const User=(props)=>{
     return(
         <div className="user">
         <h1>Name : {props.Name}</h1>
-        <h2>Location : Bijnor</h2>
+        <h2>Location : Delhi</h2>
         <h3>Contact : 1234567890</h3>
         <h3>Profession : Developer</h3>
         <h3>Company : Microsoft</h3>
-       
+        <h3>Experience : 5 years</h3>
+        
+
+        
 
     
         </div>
