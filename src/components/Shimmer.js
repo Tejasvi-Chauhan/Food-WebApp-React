0c57c90a2e15804d7eef60dc9ca45b4da4dd8423
Tejasvi-Chauhan <chauhantejasvi07@gmail.com>
2026-01-17 20:58:33 +0530

    changes in shimmer

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,11 +1,14 @@
 const ShimmerCard = () => {
   return (
     <div className="shimmer-container">
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        
+      <div className="shimmer-card">
+        <div className="shimmer-image"></div>
+        <div className="shimmer-content">
+          <div className="shimmer-title"></div>
+          <div className="shimmer-rating"></div>
+          <div className="shimmer-address"></div>
+        </div>
+      </div>
     </div>
   );
 }
